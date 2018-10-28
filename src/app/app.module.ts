592ad86bcb34384b2debd09da6dedda0ffd997b7
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-28 08:41:15 +0300

    generated a detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { GithubDetailsComponent } from './github-details/github-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    GithubDetailsComponent
   ],
   imports: [
     BrowserModule
