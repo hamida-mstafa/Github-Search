c9fbe37cf71a4abd2f3095e5a50cd440556b4f06
hamida-mstafa <hamidamstafa@gmail.com>
2018-10-30 22:59:35 +0300

    added the client id and secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   clientId:'690ab943b7bacea06a9d',
-//   clientSecrets:'186935a9b526d6ecbe27f508cc129a3c0fa0127c'
-//
-// };
+export const environment = {
+  production: false,
+  clientId:'690ab943b7bacea06a9d',
+  clientSecrets:'186935a9b526d6ecbe27f508cc129a3c0fa0127c'
+
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
